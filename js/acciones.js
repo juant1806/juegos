752cc9adf8bdd4cbff5793bb9ea9d2a36d0c88b2
juant1806 <damcb164e38@gmail.com>
2015-12-03 23:43:52 -0600

    juego1

---
+++ js/acciones.js
@@ -0,0 +1,81 @@
+// JavaScript Document
+$(document).ready(function(e) {
+document.addEventListener("deviceready",function(){
+	$("#consulta1").on("tap", function (){
+		$.ajax({
+			type:"POST", 
+			url:"http://192.168.1.68/videojuegos/buscarTodos.php",
+			error: function(){
+			 alert ('No se Puedo Conectar  a la Base de Datos');
+			}, 
+			success: function(respuesta){
+				
+			var datosjuegos = JSON.parse(respuesta);
+			//alert(datosProducto.productos[0].nombreProducto);
+		
+			for (var x= 0; x<datosjuegos.juegos.length; x++)
+			{
+				$('#todo').append('<div><div><div style="display:inline-block"><img src="http://192.168.1.68/videojuegos/recursos/imagenes/fotos/'+datosjuegos.juegos[x].clave_videojuego+'.jpg"></div><div style="display:inline-block"><h1>'+ datosjuegos.juegos[x].nombre_videojuego +' </h1><h2> '+datosjuegos.juegos[x].genero_videojuego+' </h2></div></div><div><h2> '+datosjuegos.juegos[x].clasificacion_videojuego+' </h2><h2> '+datosjuegos.juegos[x].consola_videojuego+' </h2></div></div>');
+			}
+				$(':mobile-pagecontainer').pagecontainer('change', '#Pagina1',{
+				transition: 'pop'
+			});
+			}
+		});
+			
+		});// tap boton todo
+		
+		
+		$("#btn_gen").on("tap", function (){
+		$.ajax({
+			type:"POST",
+			 
+			url:"http://192.168.1.71//buscarGenero.php",
+			data: "genero=" + $('#txtgenero').val(), 
+			error: function(){
+			 alert ('No se Puedo Conectar  a la Base de Datos');
+			}, 
+			success: function(respuestagen){
+				
+			var datosjuegos = JSON.parse(respuestagen);
+			//alert(datosProducto.productos[0].nombreProducto);
+		
+			for (var x= 0; x<datosjuegos.juegos.length; x++)
+			{
+				$('#contenidogen').append('<div><div><div style="display:inline-block"><img src="http://192.168.1.68/videojuegos/recursos/imagenes/fotos/'+datosjuegos.juegos[x].clave_videojuego+'.jpg"></div><div style="display:inline-block"><h1>'+ datosjuegos.juegos[x].nombre_videojuego +' </h1><h2> '+datosjuegos.juegos[x].genero_videojuego+' </h2></div></div><div><h2> '+datosjuegos.juegos[x].clasificacion_videojuego+' </h2><h2> '+datosjuegos.juegos[x].consola_videojuego+' </h2></div></div>');
+			}
+				$(':mobile-pagecontainer').pagecontainer('change', '#genero',{
+				transition: 'pop'
+			});
+			}
+		});
+			
+		});// tap btn_gen
+		
+		
+		$("#btn_con").on("tap", function (){
+		$.ajax({
+			type:"POST", 
+			url:"http://192.168.1.68/videojuegos/buscarConsolas.php",
+			data: "consola=" + $('#txtconsola').val(),
+			error: function(){
+			 alert ('No se Puedo Conectar  a la Base de Datos');
+			}, 
+			success: function(respuestamarca){
+				
+			var datosconsola = JSON.parse(respuestaconsola);
+			//alert(datosProducto.productos[0].nombreProducto);
+		
+			for (var x= 0; x<datosconsola.juegos.length; x++)
+			{
+				$('#contenidocon').append('<div><div><div style="display:inline-block"><img src="http://192.168.1.68/videojuegos/recursos/imagenes/fotos/'+datosconsola.juegos[x].clave_videojuego+'.jpg"></div><div style="display:inline-block"><h1>'+ datosconsola.juegos[x].nombre_videojuego +' </h1><h2> '+datosconsola.juegos[x].genero_videojuego+' </h2></div></div><div><h2> '+datosconsola.juegos[x].clasificacion_videojuego+' </h2><h2> '+datosconsola.juegos[x].consola_videojuego+' </h2></div></div>');
+			}
+				$(':mobile-pagecontainer').pagecontainer('change', '#consola',{
+				transition: 'pop'
+			});
+			}
+		});
+			
+		});// tap btn_con
+}); 
+});
