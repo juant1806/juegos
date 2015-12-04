06970f2c21ae3778ed435637edcac577f48a005a
juant1806 <damcb164e38@gmail.com>
2015-12-03 23:56:54 -0600

    juego2

--- js/acciones.js
+++ js/acciones.js
@@ -15,7 +15,7 @@ document.addEventListener("deviceready",function(){
 		
 			for (var x= 0; x<datosjuegos.juegos.length; x++)
 			{
-				$('#todo').append('<div><div><div style="display:inline-block"><img src="http://192.168.1.68/videojuegos/recursos/imagenes/fotos/'+datosjuegos.juegos[x].clave_videojuego+'.jpg"></div><div style="display:inline-block"><h1>'+ datosjuegos.juegos[x].nombre_videojuego +' </h1><h2> '+datosjuegos.juegos[x].genero_videojuego+' </h2></div></div><div><h2> '+datosjuegos.juegos[x].clasificacion_videojuego+' </h2><h2> '+datosjuegos.juegos[x].consola_videojuego+' </h2></div></div>');
+				$('#todo').append('<div><div><div style="display:inline-block"><img src="http://192.168.1.68/videojuegos/recursos/imagenes/fotos/'+datosjuegos.juegos[x].clave_videojuego+'.jpg" width="100"></div><div style="display:inline-block"><h1>'+ datosjuegos.juegos[x].nombre_videojuego +' </h1><h2> '+datosjuegos.juegos[x].genero_videojuego+' </h2></div></div><div><h2> '+datosjuegos.juegos[x].clasificacion_videojuego+' </h2><h2> '+datosjuegos.juegos[x].consola_videojuego+' </h2></div></div>');
 			}
 				$(':mobile-pagecontainer').pagecontainer('change', '#Pagina1',{
 				transition: 'pop'
@@ -30,7 +30,7 @@ document.addEventListener("deviceready",function(){
 		$.ajax({
 			type:"POST",
 			 
-			url:"http://192.168.1.71//buscarGenero.php",
+			url:"http://192.168.1.71/videojuegos/buscarGenero.php",
 			data: "genero=" + $('#txtgenero').val(), 
 			error: function(){
 			 alert ('No se Puedo Conectar  a la Base de Datos');
@@ -40,9 +40,9 @@ document.addEventListener("deviceready",function(){
 			var datosjuegos = JSON.parse(respuestagen);
 			//alert(datosProducto.productos[0].nombreProducto);
 		
-			for (var x= 0; x<datosjuegos.juegos.length; x++)
+			for (var x= 0; x<datosjuegos.genero.length; x++)
 			{
-				$('#contenidogen').append('<div><div><div style="display:inline-block"><img src="http://192.168.1.68/videojuegos/recursos/imagenes/fotos/'+datosjuegos.juegos[x].clave_videojuego+'.jpg"></div><div style="display:inline-block"><h1>'+ datosjuegos.juegos[x].nombre_videojuego +' </h1><h2> '+datosjuegos.juegos[x].genero_videojuego+' </h2></div></div><div><h2> '+datosjuegos.juegos[x].clasificacion_videojuego+' </h2><h2> '+datosjuegos.juegos[x].consola_videojuego+' </h2></div></div>');
+				$('#contenidogen').append('<div><div><div style="display:inline-block"><img src="http://192.168.1.68/videojuegos/recursos/imagenes/fotos/'+datosjuegos.genero[x].clave_videojuego+'.jpg" width="100"></div><div style="display:inline-block"><h1>'+ datosjuegos.genero[x].nombre_videojuego +' </h1><h2> '+datosjuegos.genero[x].genero_videojuego+' </h2></div></div><div><h2> '+datosjuegos.genero[x].clasificacion_videojuego+' </h2><h2> '+datosjuegos.genero[x].consola_videojuego+' </h2></div></div>');
 			}
 				$(':mobile-pagecontainer').pagecontainer('change', '#genero',{
 				transition: 'pop'
@@ -66,9 +66,9 @@ document.addEventListener("deviceready",function(){
 			var datosconsola = JSON.parse(respuestaconsola);
 			//alert(datosProducto.productos[0].nombreProducto);
 		
-			for (var x= 0; x<datosconsola.juegos.length; x++)
+			for (var x= 0; x<datosconsola.consola.length; x++)
 			{
-				$('#contenidocon').append('<div><div><div style="display:inline-block"><img src="http://192.168.1.68/videojuegos/recursos/imagenes/fotos/'+datosconsola.juegos[x].clave_videojuego+'.jpg"></div><div style="display:inline-block"><h1>'+ datosconsola.juegos[x].nombre_videojuego +' </h1><h2> '+datosconsola.juegos[x].genero_videojuego+' </h2></div></div><div><h2> '+datosconsola.juegos[x].clasificacion_videojuego+' </h2><h2> '+datosconsola.juegos[x].consola_videojuego+' </h2></div></div>');
+				$('#contenidocon').append('<div><div><div style="display:inline-block"><img src="http://192.168.1.68/videojuegos/recursos/imagenes/fotos/'+datosconsola.consola[x].clave_videojuego+'.jpg" width="100"></div><div style="display:inline-block"><h1>'+ datosconsola.consola[x].nombre_videojuego +' </h1><h2> '+datosconsola.consola[x].genero_videojuego+' </h2></div></div><div><h2> '+datosconsola.consola[x].clasificacion_videojuego+' </h2><h2> '+datosconsola.consola[x].consola_videojuego+' </h2></div></div>');
 			}
 				$(':mobile-pagecontainer').pagecontainer('change', '#consola',{
 				transition: 'pop'
